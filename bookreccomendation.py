4de3f30517bd88aa5b0961d635eda72624e5b069
clare0901 <clarerebello0901@gmail.com>
2021-10-20 16:11:28 +0530

    update

--- bookreccomendation.py
+++ bookreccomendation.py
@@ -1,7 +1,6 @@
 from re import A
 import streamlit as st
-
-import numpy as np 
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -9,7 +8,10 @@ from sklearn.cluster import KMeans
 from sklearn import neighbors
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-
+import base64
+from skimage import io
+import cv2
+from sklearn.preprocessing import MinMaxScaler
 
 df = pd.read_csv('books1.csv',error_bad_lines = False)
 
@@ -71,7 +73,6 @@ sns.barplot(x="average_rating", y="title", data=data, palette='inferno')
 # ax = sns.relplot(x="average_rating", y="num_pages", data = df, color = 'red',sizes=(100, 200), height=7, marker='o')
 # ax.set_axis_labels("Average Rating", "Number of Pages")
 
-import base64
 
 main_bg = "sample2.jpg"
 main_bg_ext = "jpg"
@@ -82,10 +83,10 @@ st.markdown(
     .reportview-container {{
         background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
         background-size:cover;
-        
-        
+
+
     }}
-   
+
     </style>
     """,
     unsafe_allow_html=True
@@ -105,55 +106,50 @@ rating_df = pd.get_dummies(df2['rating_between'])
 
 rating_df = rating_df.replace(np.nan, 0)
 
-features = pd.concat([rating_df,  
-                      df2['average_rating'], 
+features = pd.concat([rating_df,
+                      df2['average_rating'],
                       df2['ratings_count']], axis=1)
 
-from sklearn.preprocessing import MinMaxScaler
 min_max_scaler = MinMaxScaler()
 features = min_max_scaler.fit_transform(features)
-
 
 model = neighbors.NearestNeighbors(n_neighbors=6, algorithm='ball_tree')
 model.fit(features)
 dist, idlist = model.kneighbors(features)
 
-st.title('Book Recommendation')
-
-from skimage import io
-import cv2
+st.title('Book Recommendations !!! ')
 
 def crop(img, center, width, height):
     return cv2.getRectSubPix(img, (width, height), center)
-    
+
 def BookRecommender(book_name):
     book_list_name = []
     dictionary = dict()
-    
+
     cols = st.columns(3)
-    
+
     book_id = df2[df2['title'] == book_name].index
     try:
         id = book_id[0]
     except:
         id = 0
-    
+
     if (id == 0):
-    #   st.write("Book not Found ! Sorry :(")
-        st.write("")
+      st.write("Book not Found ! Sorry :(")
+        # st.write("")
     else:
         book_id = book_id[0]
         ex = []
         i=0
         for newid in idlist[book_id]:
-            
-            book_list_name = []         
+
+            book_list_name = []
             name = df2.iloc[newid]['title']
             url = df2.iloc[newid]['thumbnail']
             cols[i].write(name)
 
-            if ( url != 0):  
-                image = io.imread(url)   
+            if ( url != 0):
+                image = io.imread(url)
                 img = cv2.imshow('image',image)
                 cropped = crop(image,center=(70, 85), width=150, height=240)
                 cols[i].image(cropped)
@@ -162,7 +158,6 @@ def BookRecommender(book_name):
                 else:
                     i=i+1
             else:
-                
                 cols[i].image("imagenotfound.jpg",width=150)
                 if(i==2):
                     i=0
@@ -170,8 +165,4 @@ def BookRecommender(book_name):
                     i=i+1
 
 book_name = st.text_input("Book Name:", "")
-
-
-
-
 BookNames = BookRecommender(book_name)

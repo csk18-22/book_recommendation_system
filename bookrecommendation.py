0b6575d97921f1c9e1728d3952a53e50ceb70cfa
clare0901 <clarerebello0901@gmail.com>
2021-10-20 23:12:40 +0530

    update

--- bookrecommendation.py
+++ bookrecommendation.py
@@ -1,22 +1,16 @@
 import streamlit as st
-import numpy as np 
+import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans
 from sklearn import neighbors
-from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from skimage import io
 import cv2
-
 import base64
 
-
 df = pd.read_csv('books.csv',error_bad_lines = False)
-
 df = df.fillna(0)
-
 
 # making copy of dataframe
 df2 = df.copy()
@@ -28,12 +22,9 @@ df2.loc[ (df2['average_rating'] > 3) & (df2['average_rating'] <= 4), 'rating_bet
 df2.loc[ (df2['average_rating'] > 4) & (df2['average_rating'] <= 5), 'rating_between'] = "between 4 and 5"
 
 rating_df = pd.get_dummies(df2['rating_between'])
-
 rating_df = rating_df.replace(np.nan, 0)
 
-features = pd.concat([rating_df,  
-                      df2['average_rating'], 
-                      df2['ratings_count']], axis=1)
+features = pd.concat([rating_df, df2['average_rating'], df2['ratings_count']], axis=1)
 
 min_max_scaler = MinMaxScaler()
 features = min_max_scaler.fit_transform(features)
@@ -45,24 +36,23 @@ dist, idlist = model.kneighbors(features)
 
 st.title('Book Recommendation System')
 
-
-
+# crop image function
 def crop(img, center, width, height):
     return cv2.getRectSubPix(img, (width, height), center)
-    
+
 # main function
 def BookRecommender(book_name):
     book_list_name = []
     dictionary = dict()
-    
+
     cols = st.columns(3)
-    
+
     book_id = df2[df2['title'] == book_name].index
     try:
         id = book_id[0]
     except:
         id = 0
-    
+
     if (id == 0):
         st.write("")
     else:
@@ -70,14 +60,14 @@ def BookRecommender(book_name):
         ex = []
         i=0
         for newid in idlist[book_id]:
-            
-            book_list_name = []         
+
+            book_list_name = []
             name = df2.iloc[newid]['title']
             url = df2.iloc[newid]['thumbnail']
             cols[i].write(name)
 
-            if ( url != 0):  
-                image = io.imread(url)   
+            if ( url != 0):
+                image = io.imread(url)
                 img = cv2.imshow('image',image)
                 cropped = crop(image,center=(70, 85), width=150, height=240)
                 cols[i].image(cropped)
@@ -86,7 +76,6 @@ def BookRecommender(book_name):
                 else:
                     i=i+1
             else:
-                
                 cols[i].image("imagenotfound.jpg",width=150)
                 if(i==2):
                     i=0
@@ -97,15 +86,13 @@ def BookRecommender(book_name):
 st.markdown("""
 <style>
 body {
-  color: red; 
+  color: red;
 }
 </style>
     """, unsafe_allow_html=True)
 
 main_bg = "bgimg.jpg"
 main_bg_ext = "jpg"
-
-
 
 st.markdown(
     f"""
@@ -114,13 +101,11 @@ st.markdown(
         background: url(data:image/{main_bg_ext};base64,{base64.b64encode(open(main_bg, "rb").read()).decode()});
         background-size:cover;
     }}
-   
+
     </style>
     """,
     unsafe_allow_html=True
 )
 
-
 book_name = st.text_input("Book Name:", "")
-
 BookNames = BookRecommender(book_name)
